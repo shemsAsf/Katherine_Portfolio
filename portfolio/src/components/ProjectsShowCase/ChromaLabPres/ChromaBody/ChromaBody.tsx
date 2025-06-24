a7b7d93692c5cba9df0aa655b8795a755b54271a
shemsasfouri <shems.asfouri@gmail.com>
2025-06-24 22:10:24 +0200

    Added navigation box

---
+++ portfolio/src/components/ProjectsShowCase/ChromaLabPres/ChromaBody/ChromaBody.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import "./ChromaBody.css";
+
+export default function ChromaBody() {
+
+    return (
+        <div className="chroma-body">
+            
+        </div>
+    );
+}
--- portfolio/src/components/layout/Header/Header.tsx
+++ portfolio/src/components/layout/Header/Header.tsx
@@ -2,16 +2,47 @@
 
 import "./Header.css";
 import { useNavigate } from 'react-router-dom';
+import { useState, useEffect, useRef } from 'react';
 
 export default function Header() {
 	const navigate = useNavigate();
+	const [showNav, setShowNav] = useState(false);
+	const navRef = useRef<HTMLDivElement>(null);
+	const logoRef = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		function handleClickOutside(event: MouseEvent) {
+			if (
+				showNav &&
+				navRef.current &&
+				!navRef.current.contains(event.target as Node) &&
+				logoRef.current &&
+				!logoRef.current.contains(event.target as Node)
+			) {
+				setShowNav(false);
+			}
+		}
+
+		document.addEventListener("mousedown", handleClickOutside);
+		return () => document.removeEventListener("mousedown", handleClickOutside);
+	}, [showNav]);
 
 	return (
-	<div className="header-container">
-		<header>
-			<p onClick={() => navigate('/')}>ekaterina potapova</p>
-			<img src="/Img/BB-bird-2.png" className="header-logo" alt="logo" />
-		</header>
-	</div>
+		<div className="header-container">
+			<header>
+				<p onClick={() => navigate('/')}>ekaterina potapova</p>
+				<div className="logo-container" onClick={() => setShowNav(!showNav)} ref={logoRef}>
+					<img src="/Img/BB-bird-2.png" className="header-logo" alt="logo" />
+				</div>
+				{showNav && (
+					<div className="nav-box" ref={navRef}>
+						<button onClick={() => navigate('/Contact')}>Contact me</button>
+						<button onClick={() => navigate('/portfolio')}>Portfolio</button>
+						<button onClick={() => navigate('/CV')}>CV page</button>
+						<button onClick={() => navigate('/pigeon')}>Why pigeon?</button>
+					</div>
+				)}
+			</header>
+		</div>
 	);
 }
